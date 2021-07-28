99862863bc300d5949f369dcbd56a434d957a5b0
AndrewMMickel <andrew.m.mickel@gmail.com>
2021-07-28 12:56:26 -0700

    create models + controllers

---
+++ AnimalClient/Controllers/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AnimalClient.Models;
+
+namespace AnimalClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Animals");
+        }
+    }
+}
